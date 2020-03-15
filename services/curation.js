8c547d8c064402cabb4b48f5c07afef5080202d2
emmaodia <odia.emma@gmail.com>
2020-03-16 00:50:16 +0100

    Update curation.js.

--- services/curation.js
+++ services/curation.js
@@ -44,7 +44,7 @@ module.exports = class Curation {
             [
               Response.genWebUrlButton(
                 i18n.__("curation.wash-hands-subtitle"),
-                `${config.shopUrl}/styles/wash-hands.mp4`,
+                `${config.appUrl}/styles/wash-hands.mp4`,
               ),
               Response.genPostbackButton(
                 i18n.__("curation.show"),
