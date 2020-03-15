9789bbe863d32f1000f6b1f2b51e90194f639382
emmaodia <odia.emma@gmail.com>
2020-03-16 00:07:31 +0100

    Update curation.js.

--- services/curation.js
+++ services/curation.js
@@ -41,16 +41,16 @@ module.exports = class Curation {
             i18n.__("curation.wash-hands"),
             // i18n.__("curation.subtitle"),
             // `https://www.youtube.com/watch?v=y7e8nM0JAz0`,
-            // [
-            //   Response.genWebUrlButton(
-            //     i18n.__("curation.wash-hands-subtitle"),
-            //     `https://www.youtube.com/watch?v=y7e8nM0JAz0`
-            //   ),
-            //   Response.genPostbackButton(
-            //     i18n.__("curation.show"),
-            //     "CURATION_OTHER_STYLE"
-            //   )
-            // ]
+            [
+              Response.genWebUrlButton(
+                i18n.__("curation.wash-hands-subtitle"),
+                `https://www.youtube.com/watch?v=y7e8nM0JAz0`
+              ),
+              Response.genPostbackButton(
+                i18n.__("curation.show"),
+                "CURATION_OTHER_STYLE"
+              )
+            ]
           ),  
           Response.genQuickReply(i18n.__("curation.show"), [
             {
