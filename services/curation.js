d2377d601f140447fe1c964c2ac30740cbf694ec
emmaodia <odia.emma@gmail.com>
2020-03-16 17:48:09 +0100

    Update curation.js and json file.

--- services/curation.js
+++ services/curation.js
@@ -60,6 +60,14 @@ module.exports = class Curation {
             {
               title: i18n.__("curation.stay-home"),
               payload: "CURATION_SOMEONE_ELSE"
+            },
+            {
+              title: i18n.__("curation.sales"),
+              payload: "CURATION_SOMEONE_ELSE"
+            },
+            {
+              title: i18n.__("curation.travel"),
+              payload: "CURATION_SOMEONE_ELSE"
             }
           ]),
         ]
